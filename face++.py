f6f4dc7c28522b8cc3240f0c05c9fbee2c34c6f9
Matt Zheng <mattzheng@yeah.net>
2018-03-06 17:47:19 +0800

    Update face++.py

--- face++.py
+++ face++.py
@@ -70,10 +70,80 @@ api = API(key, secret , srv = 'https://api-cn.faceplusplus.com/humanbodypp/v1/de
 api.detect(image_file = File(r"C:/Desktop/1.jpg"))
 
 
+# -------------------face++ 场景识别 + 人体语义分割 + OCR识别---------------------------
+import sys
+sys.path.append('/data1/research/matt/face++/python-sdk')
+from facepp import API, File
+import urllib2,time
 
+def faceplus_api(key,secret,filepath,http_url):
 
+    boundary = '----------%s' % hex(int(time.time() * 1000))
+    data = []
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_key')
+    data.append(key)
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_secret')
+    data.append(secret)
+    data.append('--%s' % boundary)
+    fr=open(filepath,'rb')
+    data.append('Content-Disposition: form-data; name="%s"; filename=" "' % 'image_file')
+    data.append('Content-Type: %s\r\n' % 'application/octet-stream')
+    data.append(fr.read())
+    fr.close()
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'return_landmark')
+    data.append('1')
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'return_attributes')
+    data.append("gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus")
+    data.append('--%s--\r\n' % boundary)
 
+    http_body='\r\n'.join(data)
+    #buld http request
+    req=urllib2.Request(http_url)
+    #header
+    req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
+    req.add_data(http_body)
+    qrcont = None
+    try:
+        #req.add_header('Referer','http://remotserver.com/')
+        #post data to server
+        resp = urllib2.urlopen(req, timeout=5)
+        #get response
+        qrcont=resp.read()
+        #print qrcont
 
+    except urllib2.HTTPError as e:
+        print e.read()
+        
+    return qrcont
 
+if __name__ == "__main__"
+	# 场景识别
+	http_url='https://api-cn.faceplusplus.com/imagepp/beta/detectsceneandobject'
+	API_KEY = "xxx"
+	API_SECRET = "xxx"
+	face_one = 'http://bj-mc-prod-asset.oss-cn-beijing.aliyuncs.com/mc-official/images/face/demo-pic11.jpg'
+	face_two = '/../Hydrangeas.jpg'
+	result = faceplus_api( API_KEY,API_SECRET, face_two ,http_url)
+	
+	# 人体语义分割
+	http_url='https://api-cn.faceplusplus.com/humanbodypp/v1/segment'
+	API_KEY = "xxx"
+	API_SECRET = "xxx"
+	#face_one = 'http://bj-mc-prod-asset.oss-cn-beijing.aliyuncs.com/mc-official/images/face/demo-pic11.jpg'
+	face_two = '/data1/research/matt/face++/out2.jpg'
+	result = faceplus_api( API_KEY,API_SECRET, face_two ,http_url)
+
+	# OCR识别
+	http_url='https://api-cn.faceplusplus.com/imagepp/v1/recognizetext'
+	API_KEY = "xxx"
+	API_SECRET = "xxx"
+	#face_one = 'http://bj-mc-prod-asset.oss-cn-beijing.aliyuncs.com/mc-official/images/face/demo-pic11.jpg'
+	face_two = '/data1/research/matt/face++/out3.jpg'
+	ocr_result = faceplus_api( API_KEY,API_SECRET, face_two ,http_url) 
+	print ocr_result
 
 
